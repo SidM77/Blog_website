0b5cd19bdf1feedb8ea9b9d60766315843f74cc9
SidM77 <siddanth.manoj@gmail.com>
2023-07-23 21:38:27 +0530

    just minor changes, np actual

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const mongoose = require('mongoose')
 const express = require('express')
-const app = express();
+const app = express()
 const articleRouter = require('./routes/articles')
-const Article=require('./models/article')
-const methodOverride=require('method-override')
+const Article = require('./models/article')
+const methodOverride = require('method-override')
 mongoose.connect('mongodb://127.0.0.1:27017/blog')
-app.listen(5000);
+app.listen(5000)
 
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
-
-
-
-
-app.get('/', async(req, res) => {
-  const articles= await Article.find().sort({createdAt:'desc'})
-  res.render('articles/index',{articles:articles});
-});
-
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use(methodOverride('_method'))
 
-app.set('view engine', 'ejs');
-app.use('/articles', articleRouter);
+app.set('view engine', 'ejs')
+app.use('/articles', articleRouter)
